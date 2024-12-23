3071a51cd8c2a6640f4f7154f377e504424e0dfa
Eshiunm <eshiunm@gmail.com>
2024-12-23 23:44:18 +0800

    feat: finish rolling dice game

--- script.js
+++ script.js
@@ -46,4 +46,53 @@ btnRoll.addEventListener('click', () => {
 });
 
 // Holding current score functionality （保留當前分數功能）
-btnHold.addEventListener('click', () => {});
+btnHold.addEventListener('click', () => {
+  // 1. Add current score to active player's score
+  const currentScore = document.getElementById(
+    `current--${activePlayerNumber}`
+  );
+
+  const activePlayerScore = document.getElementById(
+    `score--${activePlayerNumber}`
+  );
+  activePlayerScore.textContent =
+    Number(activePlayerScore.textContent) + Number(currentScore.textContent);
+  currentScore.textContent = 0;
+
+  // 2. Check if player's score is >= 100
+  if (Number(activePlayerScore.textContent) >= 100) {
+    // Finish the game
+    document
+      .querySelector(`.player--${activePlayerNumber}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayerNumber}`)
+      .classList.remove('player--active');
+    diceEl.classList.add('hidden');
+    btnRoll.disabled = true;
+    btnHold.disabled = true;
+  }
+
+  // 3. Switch to the next player
+  currentActivePlayerScore = 0;
+  activePlayerNumber = activePlayerNumber === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+});
+
+// Resetting the game
+btnNewGame.addEventListener('click', () => {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  btnRoll.disabled = false;
+  btnHold.disabled = false;
+  currentActivePlayerScore = 0;
+  activePlayerNumber = 0;
+});
