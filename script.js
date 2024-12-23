b91cc62e199657a18f0db0de4342e347c0758fd9
Eshiunm <eshiunm@gmail.com>
2024-12-23 17:13:08 +0800

    feat:add rolling dice Event listener

--- script.js
+++ script.js
@@ -4,8 +4,18 @@
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality （擲骰子功能）
+btnRoll.addEventListener('click', () => {
+  // 1. Generate a random dice roll
+  // 2. Display dice
+  // 3 Check for rolled 1: if true, switch to ner player
+});
